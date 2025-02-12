8bd4b521b7cc1d169343575ca28b18126e274117
Lukas Schießer <lukas.schiesser@dfki.de>
2025-02-12 11:45:12 +0100

    updated srun command

--- srun.sh
+++ srun.sh
@@ -1,7 +1,7 @@
 srun \
---container-mounts=/netscratch/$USER:/netscratch/$USER,"`pwd`":"`pwd`" \
+--container-mounts=/netscratch/$USER:/netscratch/$USER,"`pwd`":"`pwd`",ds:/ds \
 --container-workdir="`pwd`" \
 --container-image=/enroot/nvcr.io_nvidia_pytorch_23.06-py3.sqsh \
 --task-prolog="`pwd`/install.sh" \
 --gpus=1 --partition=A100-40GB --mem=30000 \
-python3 train.py
+python3 train.py --config ./configs/slurm.yaml
