232609b0783a4d5f07f152f157d62a293bcc8d60
Lukas Schießer <lukas.schiesser@dfki.de>
2025-02-19 13:51:13 +0100

    changed auxiliary files

--- srun.sh
+++ srun.sh
@@ -1,7 +1,7 @@
 srun \
---container-mounts=/netscratch/$USER:/netscratch/$USER,"`pwd`":"`pwd`",ds:/ds \
+--container-mounts=/netscratch/$USER:/netscratch/$USER,"`pwd`":"`pwd`",/ds:/ds \
 --container-workdir="`pwd`" \
 --container-image=/enroot/nvcr.io_nvidia_pytorch_23.06-py3.sqsh \
 --task-prolog="`pwd`/install.sh" \
---gpus=1 --partition=A100-40GB --mem=30000 \
+--gpus=1 --partition=H100-SEE --mem=120000 --job-name=icltest \
 python3 train.py --config ./configs/slurm.yaml
