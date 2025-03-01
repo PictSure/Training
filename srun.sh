e6439c62f13dba89f73915366df65cbcd613a6be
Lukas Schießer <lukas.schiesser@dfki.de>
2025-03-01 19:14:20 +0100

    updated config files and srun commands

--- srun.sh
+++ srun.sh
@@ -1,7 +1,35 @@
+# test
 srun \
 --container-mounts=/netscratch/$USER:/netscratch/$USER,"`pwd`":"`pwd`",/ds:/ds \
 --container-workdir="`pwd`" \
 --container-image=/enroot/nvcr.io_nvidia_pytorch_23.06-py3.sqsh \
 --task-prolog="`pwd`/install.sh" \
---gpus=1 --partition=H100-SEE --mem=120000 --job-name=icltest \
+--gpus=1 --cpus-per-gpu=4 --partition=H100-SEE --mem=190000 --job-name=icltest \
 python3 train.py --config ./configs/slurm.yaml
+
+# S-Model
+srun \
+--container-mounts=/netscratch/$USER:/netscratch/$USER,"`pwd`":"`pwd`",/ds:/ds \
+--container-workdir="`pwd`" \
+--container-image=/enroot/nvcr.io_nvidia_pytorch_23.06-py3.sqsh \
+--task-prolog="`pwd`/install.sh" \
+--gpus=1 --cpus-per-gpu=16 --partition=H100-SEE --mem=205000 --job-name=pictsures --time=3-00:00 \
+python3 train.py --config ./configs/models/PictSureS.yaml
+
+# M-Model
+srun \
+--container-mounts=/netscratch/$USER:/netscratch/$USER,"`pwd`":"`pwd`",/ds:/ds \
+--container-workdir="`pwd`" \
+--container-image=/enroot/nvcr.io_nvidia_pytorch_23.06-py3.sqsh \
+--task-prolog="`pwd`/install.sh" \
+--gpus=1 --cpus-per-gpu=16 --partition=A100-80GB --mem=205000 --job-name=pictsurem --time=3-00:00 \
+python3 train.py --config ./configs/models/PictSureM.yaml
+
+# L-Model
+srun \
+--container-mounts=/netscratch/$USER:/netscratch/$USER,"`pwd`":"`pwd`",/ds:/ds \
+--container-workdir="`pwd`" \
+--container-image=/enroot/nvcr.io_nvidia_pytorch_23.06-py3.sqsh \
+--task-prolog="`pwd`/install.sh" \
+--gpus=1 --cpus-per-gpu=16 --partition=A100-80GB --mem=205000 --job-name=pictsurel --time=3-00:00 \
+python3 train.py --config ./configs/models/PictSureL.yaml
